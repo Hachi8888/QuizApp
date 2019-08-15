13684ac38d382feb10af308225abf99c62f1a50c
Hachi8888 <h.nana.420@gmail.com>
2019-08-15 13:03:58 +0900

    画面レイアウト修正

--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // クイズを表示するテキストビュー
+    @IBOutlet weak var showQuiz: UITextView!
+    
+    // 問題文
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+     
+    
+    
     }
+    
+
+    @IBAction func selectAnswer(_ sender: UIButton) {
+    }
+    
+    
 
 
 }
